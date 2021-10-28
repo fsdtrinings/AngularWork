f790a1da961468a7b690e9933e11403c6214f94f
fsdtrainings <javafsd.trainings@gmail.com>
2021-10-28 14:09:21 +0530

    day 1 angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopheaderComponent } from './nes/topheader/topheader.component';
+import { AppheaderComponent } from './nse/appheader/appheader.component';
+import { LinkheaderComponent } from './nse/linkheader/linkheader.component';
+import { LeftpanelComponent } from './nse/leftpanel/leftpanel.component';
+import { MainpanelComponent } from './nse/mainpanel/mainpanel.component';
+import { RightpanelComponent } from './nse/rightpanel/rightpanel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheaderComponent,
+    AppheaderComponent,
+    LinkheaderComponent,
+    LeftpanelComponent,
+    MainpanelComponent,
+    RightpanelComponent
   ],
   imports: [
     BrowserModule
